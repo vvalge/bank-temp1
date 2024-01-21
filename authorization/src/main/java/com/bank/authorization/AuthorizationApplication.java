b3b32d3bdaf26b4c2401909955a5b478bcb17a46
Xenia Ignateva <jok777@yandex.ru>
2024-01-21 23:20:51 +0300

    #3 Аутентифиация - Базовая структура

--- authorization/src/main/java/com/bank/authorization/AuthorizationApplication.java
+++ authorization/src/main/java/com/bank/authorization/AuthorizationApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 @EnableEurekaClient
 public class AuthorizationApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(AuthorizationApplication.class, args);
 
