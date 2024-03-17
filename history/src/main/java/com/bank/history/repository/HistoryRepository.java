852519277717b2d889157a6f15925e64806a4a22
WalGe <103536870+vvalge@users.noreply.github.com>
2024-03-17 20:09:00 +0300

    Delete history directory

--- history/src/main/java/com/bank/history/repository/HistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.bank.history.repository;
-
-import com.bank.history.model.HistoryEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HistoryRepository extends JpaRepository<HistoryEntity, Long> {
-}
